17358fdc1d86c586cc77551affe41ab81a5eb081
Davanon <davanon2014@gmail.com>
2017-03-31 11:10:35 +0530

    First

---
+++ src/main/java/com/iqmsoft/websocket/boot/Application.java
@@ -0,0 +1,28 @@
+package com.iqmsoft.websocket.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer implements 
+  WebApplicationInitializer
+{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+	
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+  
+}
